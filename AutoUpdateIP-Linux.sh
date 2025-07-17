d602bc316fb9ddce2bf45a858252d0f2f6f8d605
NUVO-8108GC\admin <kiennt@astec.vn>
2025-07-17 14:39:16 +0700

    1.1.1 change script linux

--- AutoUpdateIP-Linux.sh
+++ AutoUpdateIP-Linux.sh
@@ -1,4 +1,15 @@
 cd ~
-# clone the repository in Desktop
+
+git pull origin master
+
 cd Desktop/autoupdateip
-npm start
+
+if [ -f "assets/config.json" ]; then
+  npm start
+else
+  cp assets/config-template.json assets/config.json
+  echo "Please fill in the config.json file with your details."
+  echo "After filling in the config.json file, run the application again."
+  echo "Exiting..."
+  exit 1
+fi
